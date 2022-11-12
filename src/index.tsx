00a61785065efe0f744de6c51afdf438d975ce3c
youkaiantena <apple.kitaro@gmail.com>
2022-11-13 05:01:41 +0900

    exchange設定を変更
    - dedupExchange は操作の重複を排除します。同じクエリを同時に送信している場合、実際に API に送信されるのはそのうちの 1 つだけです。
    - cacheExchange は操作結果をキャッシュします。これは単なるドキュメント キャッシュであるため、GraphQL API からの結果は、それらの結果が要求された一意のクエリ + 変数の組み合わせによってキャッシュされます。
    - fetchExchange は fetch を使用して GraphQL リクエストを送信し、デフォルトでキャンセルをサポートします。

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider, Client, defaultExchanges } from 'urql';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+const cache = cacheExchange({});
 
 const client = new Client({
   url: 'http//localhost:4000',
-  exchanges: defaultExchanges
+  exchanges: [dedupExchange, cache, fetchExchange]
 });
 
 const root = ReactDOM.createRoot(
